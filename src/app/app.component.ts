8d57e8f115c24c86551bd9f38b2ab8b5dc8e230d
rubicel perez <rubicelperezcordova0913@gmail.com>
2023-06-26 10:09:48 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GGMap';
+
+  mapZoom = 18;
+  mapCenter: google.maps.LatLngLiteral = { lat: 19.4357169, lng: -99.1439294 };
+  mapOptions: google.maps.MapOptions = {
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    disableDoubleClickZoom: true,
+    draggable: true,
+    maxZoom: 20,
+    minZoom: 5,
+  };
+
+  markerInfoContent = '';
+  markerOptions: google.maps.MarkerOptions = {
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+  };
+
+  updateMarker(map: any){
+    const lat = map.latLng.lat();
+    const lng = map.latLng.lng();
+    this.mapCenter = { lat: lat, lng: lng }
+  }
 }
